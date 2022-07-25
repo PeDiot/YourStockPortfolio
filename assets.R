a1b3c400bb111fa9f2d4ef7f1da0b51f1241b9cf
piediot <pierre-emmanuel.diot@epsilon-france.com>
2022-07-25 09:05:48 +0200

    Add last ETH purchase.Ã

--- assets.R
+++ assets.R
@@ -27,6 +27,8 @@ stock_tickers_ <- structure(list(
 ) %>% 
   distinct(tickers, .keep_all = T) 
 
+stock_tickers_["META-PLATFORMS", "tickers"] <- "META"
+
 stock_tickers <- stock_tickers_$tickers
 stock_names <- rownames(stock_tickers_)
 names(stock_tickers) <- stock_names
@@ -75,10 +77,18 @@ names(crypto_tickers) <- crypto_names
 # ETFs ------------------------------------------------------------------
 
 etf_tickers <- c("^FCHI", 
-                 "^GSPC") 
+                 "^GSPC", 
+                 "AFK", 
+                 "EZA", 
+                 "BNK.PA", 
+                 "RTWO.AS") 
 
 etf_names <- c("CAC40", 
-               "S&P 500") 
+               "S&P 500", 
+               "Market Vectors Africa", 
+               "iShares MSCI South Africa", 
+               "Lyxor STOXX Europe 600 Banks", 
+               "L&G Russell 2000 US Small Cap UCITS") 
 
 names(etf_tickers) <- etf_names
 
@@ -105,7 +115,7 @@ my_tickers <- c("BTC-EUR",
                 "MANA-EUR", 
                 "COIN", 
                 "AMZN", 
-                "FB", 
+                "META", 
                 "NVDA", 
                 "GFC.PA", 
                 "SU.PA")
@@ -124,7 +134,7 @@ n_btc_tx <- 7
 btc_tx <- lapply(X = 1:n_btc_tx, 
                       FUN = function(i) paste("BTC-EUR", i, sep = "_")) %>% unlist()
 
-n_eth_tx <- 2
+n_eth_tx <- 3
 eth_tx <- lapply(X = 1:n_eth_tx, 
                   FUN = function(i) paste("ETH-EUR", i, sep = "_")) %>% unlist()
 
@@ -138,12 +148,13 @@ my_tickers_tx <- c(btc_tx,
                    "MANA-EUR", 
                    "COIN", 
                    amzn_tx, 
-                   "FB", 
+                   "META", 
                    "NVDA", 
                    "GFC.PA", 
                    "SU.PA")
 
 my_buying_dates <- c(
+  # BTC-EUR
   "2022-03-02", 
   "2022-05-30", 
   "2022-06-06", 
@@ -151,38 +162,59 @@ my_buying_dates <- c(
   "2022-06-20", 
   "2022-06-27", 
   "2022-07-11",
+  # ETH-EUR
   "2022-02-02", 
   "2022-07-05", 
+  "2022-07-25", 
+  # MATIC-EUR
   "2022-02-03", 
+  # MANA-EUR
   "2022-03-02", 
+  # COIN
   "2022-03-29",
+  # AMZN
   "2022-04-08", 
-  "2022-06-16", 
-  "2022-05-17", 
+  "2022-06-16",
+  # META
+  "2022-05-17",
+  # NVDA
   "2022-05-30", 
+  # GFC.PA
   "2022-07-01", 
+  # SU.PA
   "2022-07-04"
 )
 names(my_buying_dates) <- my_tickers_tx
 
 my_num_shares <- c(
+  # BTC-EUR
   0.00037241, 
   0.00041181, 
   0.00025110, 
   0.00031765,
   0.00041653, 
   0.00061189,
-  .00071752, 
+  0.00071752, 
+  # ETH-EUR
   0.00594658, 
   0.01296357,
+  0.00982442,
+  # MATIC-EUR
   10.7270573, 
+  # MANA-EUR
   5.77722346, 
+  # COIN
   0.10934065, 
+  # AMZN
   0.15021685, 
   0.04869812, 
+  # META
   0.20416538, 
+  # NVDA
   0.22274457, 
+  # GFC.PA
   0.334, 
+  # SU.PA
   0.1601
 )
 
